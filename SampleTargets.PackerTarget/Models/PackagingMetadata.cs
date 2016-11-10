272f4b2851cb41f0f678e243ea88d69af6122baa
Piotr Puszkiewicz <piotrp@microsoft.com>
2016-11-09 21:56:18 -0800

    Code cleanup

--- SampleTargets.PackerTarget/Models/PackagingMetadata.cs
+++ SampleTargets.PackerTarget/Models/PackagingMetadata.cs
@@ -5,13 +5,15 @@ namespace CliCommands.Packer.Task.Models
     public class PackagingMetadata
     {
         public string PackageFileName { get; set; }
+        
         public string ApplicationName { get; set; }
+
         public string ApplicationVersion {get; set; }
+
         public string PathToFiles {get; set; }
+
         public DeploymentType DeploymentType { get; set; }
 
         public Dictionary<string, string> CustomMetadata {get; set;} 
-
-
     }
 }
--- dotnet-packer/Program.cs
+++ dotnet-packer/Program.cs
@@ -22,7 +22,7 @@ namespace dotnetpacker
 
             };
 
-            var rcode = process.Start();
+            process.Start();
 
 
         }
